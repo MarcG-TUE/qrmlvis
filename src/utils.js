75e7e5a175b0d6e8852fbc8eeaf7cb9bc16778fb
Marc Geilen <m.c.w.geilen@tue.nl>
2020-07-27 20:07:11 +0200

    added access to layout

--- src/utils.js
+++ src/utils.js
@@ -4,8 +4,15 @@ import { Graph } from "../src/index.js";
 
 function runOn(elementID, model) {
     let container = document.getElementById(elementID);
-    ReactDOM.render(<Graph model={model} />, container);    
+    window.QrmlVisGraphComponent = ReactDOM.render(<Graph model={model} />, container);    
+}
+
+function getLayout() {
+    return window.QrmlVisGraphComponent.serializeLayout()
 }
 
 
-export default {doRunOn: (elementID, model)=> runOn(elementID, model)}
+export default {
+    doRunOn: (elementID, model) => runOn(elementID, model),
+    getLayout: () => getLayout()
+}
